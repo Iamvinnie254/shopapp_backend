4b52c0b6bf528954451228cb0c371e3a73bb0717
stephen <vincentstephen489@gmail.com>
2025-05-30 17:16:05 +0300

    updated

--- shop_app/models.py
+++ shop_app/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     slug = models.SlugField(blank=True, null=True)
     image = models.ImageField(upload_to='img')
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
     category = models.CharField(max_length=15, choices=CATEGORY, blank=True, null=True)
 
     def __str__(self):
